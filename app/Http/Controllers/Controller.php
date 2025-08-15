2cda33997dc60d1f048b27ebfd9118a81db239a1
Rashed Miya <132655219+rashedsmiya@users.noreply.github.com>
2025-08-15 10:10:47 +0600

    Add

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,10 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    // Pagination in index method
+    // public function index(Request $request)
+    // {
+    //     $data = $this->model->paginate(10);
+    //     return view($this->view . '.index', compact('data'));
+    // }
 }
