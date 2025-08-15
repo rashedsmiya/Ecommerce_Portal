ea24576fd13ac6079b3d1fda6a52fed4cc8a3f9a
rashedsmiya <rashedmiyaofficial@gmail.com>
2025-08-16 00:33:34 +0600

    Initial commit

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,6 @@
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+
 
 Route::get('/home', [HomeController::class, 'index']);
